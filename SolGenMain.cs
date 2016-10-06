eb953e8448723ab3ed8df59cd84979af7445de8c
shevelevs <shevelevs@users.noreply.github.com>
2016-10-06 13:18:08 -0400

    Update SolGenMain.cs

--- SolGenMain.cs
+++ SolGenMain.cs
@@ -1,6 +1,6 @@
 #region Copyright MIT License
 /*
- * Copyright © 2016 Sergey Shevelev, François St-Arnaud and John Wood
+ * Copyright Â© 2016 Sergey Shevelev, FranÃ§ois St-Arnaud and John Wood
  * 
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -23,13 +23,16 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
  *
- * Based on work by François St-Arnaud (http://codeproject.com/SolGen)
+ * Based on work by FranÃ§ois St-Arnaud (http://codeproject.com/SolGen)
  */
 #endregion
 
 using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace SolGen
 {
@@ -38,17 +41,54 @@ namespace SolGen
     /// </summary>
     internal class SolGenMain
     {
-        private static void Main(string[] args)
+        private const string GeneratedSolutionFileNameSuffixConfigPropertyName = "GeneratedSolutionFileNameSuffix";
+        private const string BuildConfigurationsConfigPropertyName = "BuildConfigurations";
+
+        private readonly Dictionary<string, string> _args;
+
+        public static void Main(string[] args)
         {
+            SolGenMain program = new SolGenMain(args);
+            program.Run();
+        }
+
+        private SolGenMain(string[] args)
+        {
+            _args = ParseArgs(args);
+        }
+
+        private void Run()
+        {
+            string buildConfigurationsString = GetArg("configs", ConfigurationManager.AppSettings[BuildConfigurationsConfigPropertyName] ?? string.Empty);
+            string [] buildConfigurations = buildConfigurationsString.Split(new [] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
             try
             {
                 SolutionMaker maker;
                 string solutionPath;
-                if (args.Length == 1)
+
+                if (GetArg("h", string.Empty) != string.Empty)
                 {
-                    string filepath = Path.GetFullPath(args[0]);
-                    solutionPath = Path.ChangeExtension(filepath, "sln");
-                    maker = new SolutionMaker(solutionPath);
+                    Console.WriteLine("Usage: solgen [/configs:<comma separated list of build configurations to generate>] [project file]\n");
+                    Console.WriteLine("Examples:");
+                    Console.WriteLine("          solgen");
+                    Console.WriteLine("                 will search for all project files recursively from current directory and add them and dependencies to the generated solution");
+                    Console.WriteLine("          solgen myproject.csproj");
+                    Console.WriteLine("                 will add myproject.csproj and dependencies to the generated solution");
+                    Console.WriteLine("          solgen myproject.proj");
+                    Console.WriteLine("                 will add myproject.proj and dependencies to the generated solution");
+                    Console.WriteLine("          solgen /configx:x64 myproject.csproj");
+                    Console.WriteLine("                 will add myproject.csproj and dependencies to the generated solution with build configuration x64");
+                    Console.WriteLine("          solgen /configx:x64,x86 myproject.csproj");
+                    Console.WriteLine("                 will add myproject.csproj and dependencies to the generated solution with build configurations x64 and x86");
+                    return;
+                }
+
+                if (GetArg("", string.Empty) != string.Empty)
+                {
+                    string filepath = Path.GetFullPath(GetArg("", string.Empty));
+                    solutionPath = GetSolutionFileName(Path.GetDirectoryName(filepath), Path.GetFileNameWithoutExtension(filepath));
+                    maker = new SolutionMaker(solutionPath, buildConfigurations);
                     maker.AddProject(filepath);
                 }
                 else
@@ -61,15 +101,16 @@ namespace SolGen
 
                     if (projectFiles.Length == 1)
                     {
-                        solutionPath = Path.ChangeExtension(Path.GetFullPath(projectFiles[0]), "sln");
+                        string filePath = Path.GetFullPath(projectFiles[0]);
+                        solutionPath = GetSolutionFileName(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath));
                     }
                     else
                     {
                         string fileName = Path.GetFileName(Environment.CurrentDirectory);
-                        solutionPath = Path.Combine(Environment.CurrentDirectory, fileName) + ".sln";
+                        solutionPath = GetSolutionFileName(Environment.CurrentDirectory, fileName);
                     }
 
-                    maker = new SolutionMaker(solutionPath);
+                    maker = new SolutionMaker(solutionPath, buildConfigurations);
                     foreach (string projectFile in projectFiles)
                     {
                         maker.AddProject(Path.GetFullPath(projectFile));
@@ -87,6 +128,52 @@ namespace SolGen
                 Console.WriteLine("Exception: " + e.Message + " (" + e.GetType().Name + ")");
                 Console.WriteLine(e.StackTrace);
             }
+        }
+
+        private static string GetSolutionFileName(string directory, string filename)
+        {
+            string fileNameSuffix = ConfigurationManager.AppSettings[GeneratedSolutionFileNameSuffixConfigPropertyName] ?? string.Empty;
+            
+            return Path.Combine(directory, filename + fileNameSuffix + ".sln");
+        }
+
+        private T GetArg<T>(string argName, T defaultValue)
+        {
+            string stringValue;
+
+            if (_args.TryGetValue(argName, out stringValue))
+            {
+                return (T)Convert.ChangeType(stringValue, typeof(T));
+            }
+
+            return defaultValue;
+        }
+
+        private T GetArg<T>(string argName)
+        {
+            return (T)Convert.ChangeType(_args[argName], typeof(T));
+        }
+
+        private static Dictionary<string, string> ParseArgs(string[] args)
+        {
+            var ret = new Dictionary<string, string>();
+
+            foreach (string arg in args)
+            {
+                Match m = Regex.Match(arg, "^/(?<name>[a-zA-Z_0-9]+)(:(?<value>.*))?$");
+
+                if (m.Success)
+                {
+                    string value = m.Groups["value"].Success ? m.Groups["value"].Value : "true";
+                    ret[m.Groups["name"].Value.ToLower()] = value;
+                }
+                else
+                {
+                    ret[""] = arg;
+                }
+            }
+
+            return ret;
         }
     }
 }
